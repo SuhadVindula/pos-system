2c7a9c4d7a30b697440c2a71746a53b980acd8bc
Suhad Rajapaksha <suhadrajapaksha@gmail.com>
2023-05-26 03:10:24 +0530

    Validation Logic

--- front-end/js/manage-items.js
+++ front-end/js/manage-items.js
@@ -1,5 +1,10 @@
 const tbodyElm = $('tbl-items tbody');
 const modalElm = $('new-item-modal');
+const txtItemCode = $("#txt-item-code");
+const txtDescription = $("#txt-description");
+const txtUnitPrice = $("#txt-unit-price");
+const txtStock = $("#txt-stock");
+const btnSave = $("#btn-save");
 
 
 tbodyElm.empty();
@@ -14,9 +19,55 @@ function generateNewItemCode(){
         return `C${newId.toString().padStart(3,'0')}`;
 
     }
-}
+};
+
+[txtStock, txtDescription, txtStock].forEach(txtElm =>
+    $(txtElm).addClass('animate__animated'));
 
 modalElm.on("show.bs.modal",()=>{
     txtId.val(generateNewItemCode());
 
-})
+});
+
+btnSave.on('click', () => {
+    if (!validateData()) {
+        return false;
+    }
+});
+
+function validateData() {
+    const stock = txtStock.val().trim();
+    const unitPrice = txtUnitPrice.val().trim();
+    const description = txtDescription.val().trim();
+    let valid = true;
+    resetForm();
+
+    if (!stock) {
+        valid = invalidate(txtStock, "Address can't be empty");
+    } else if (!/^\d+$/.test(stock)) {
+        valid = invalidate(txtStock, 'Invalid Stock');
+    }
+
+    if (!unitPrice) {
+        valid = invalidate(txtUnitPrice, "UnitPrice number can't be empty");
+    } else if (!/^\d{1,3}(,?\d{3})*(\.\d{1,2})?$/.test(unitPrice)) {
+        valid = invalidate(txtUnitPrice, 'Invalid Unit Price number');
+    }
+
+    if (!description) {
+        valid = invalidate(txtDescription, "Description can't be empty");
+    } else if (!/^[A-Za-z ]+$/.test(name)) {
+        valid = invalidate(txtDescription, "Invalid Description");
+    }
+
+    return valid;
+}
+
+function invalidate(txt, msg) {
+    setTimeout(() => txt.addClass('is-invalid animate__shakeX'), 0);
+    txt.trigger('select');
+    txt.next().text(msg);
+    return false;
+}
+
+
